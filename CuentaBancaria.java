0c4f8ed55aed3e0278b872f7ace15458a928d8fe
Dante_Gennari <soydante@yahoo.com>
2023-03-22 11:44:44 +0100

    Cuenta Dante Gennari introducida en el main

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -3,6 +3,7 @@ class TestCuentaBancaria
 	public static void main(String args[])
 	{
 		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100,0);
+		CuentaBancaria cuenta2 = new CuentaBancaria("0123456789","Dante Gennari",1000,5);
 		System.out.println("Saldo = " + cuenta.getSaldo());
 		cuenta.reintegro(50);
 		cuenta.ingreso(100);
