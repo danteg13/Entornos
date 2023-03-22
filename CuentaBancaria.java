7e11f2d8f4cdfbc950d052a74a9a2df350e98c77
Dante_Gennari <soydante@yahoo.com>
2023-03-22 12:05:15 +0100

    modificacion en el main parte 2

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -1,8 +1,10 @@
+//modificacion para el ejercicio de entornos git
 class TestCuentaBancaria
 {
 	public static void main(String args[])
 	{
 		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100,0);
+		CuentaBancaria cuenta3= new CuentaBancaria("0987654","dante gennari",2000,0);
 		System.out.println("Saldo = " + cuenta.getSaldo());
 		cuenta.reintegro(50);
 		cuenta.ingreso(100);
